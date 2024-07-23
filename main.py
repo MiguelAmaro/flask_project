50a58a86c951d29381bfaa1ae3e8e35ea450c20d
MiguelAmaro <miguelamaro100@gmail.com>
2024-07-23 16:38:51 -0400

    added page naviagation and renamed some stuff; used flask sessions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField, RadioField, validators
+from flask import Flask, render_template, request, session
+from flask_wtf import FlaskForm
 import requests
 import json
 import os
@@ -15,18 +15,22 @@ app.config['SECRET_KEY'] = os.urandom(16)
 # CLASSES 
 # ////////////////////////////////////////
 class SimpleForm(FlaskForm):
-    title        = StringField('Title', [validators.DataRequired()])
-    lang         = RadioField("Lang", choices=[("native","jp"),("romaji", "en")])
-    titleperpage = IntegerField("TitlePerPage", [validators.NumberRange(min=1, max=10)])
-    submit       = SubmitField('Submit')
+    title            = StringField('Title', [validators.DataRequired()])
+    lang             = RadioField("Lang", choices=[("native","jp"),("romaji", "en")])
+    page_nav         = SelectField('Page Navigation', choices=[(1, 'Next Page'), (-1, 'Previous Page')])
+    page_title_count = IntegerField("TitlePerPage", [validators.NumberRange(min=4, max=4*25)])
+    submit           = SubmitField('Submit')
 
 # ////////////////////////////////////////
 # FUNCTIONS
 # ////////////////////////////////////////
-
+def clamp(value, min_value, max_value):
+    value = min(value, max_value)
+    value = max(value, min_value)
+    return value
 # ////////////////////////////////////////
 # HELPERS ////////////////////////////////
-def api_get_data(search_term, page_title_count):
+def api_get_data(search_term, page_title_count, page_number):
     grapql_query = '''
     query ($id: Int, $page: Int, $perPage: Int, $search: String) {
         Page (page: $page, perPage: $perPage) {
@@ -53,9 +57,12 @@ def api_get_data(search_term, page_title_count):
         }
     }
     '''
+    print("search: ", search_term)
+    print("page #: ", page_number)
+    print("titl per page: ", page_title_count)
     variables = {
         'search': search_term,
-        'page': 1,
+        'page': page_number,
         'perPage': page_title_count
     }
     url = 'https://graphql.anilist.co'
@@ -67,23 +74,38 @@ def api_get_data(search_term, page_title_count):
 # ////////////////////////////////////////
 # ROUTES /////////////////////////////////
 @app.route("/", methods=['GET', 'POST'])
-def index():
-    title_entered = None
-    lang_entered  = "native"
-    titlecount_entered = 1
+def index():  
+    if 'current_page_number' not in session:
+        session['current_page_number']   = 0
+    if 'current_page_maxcount' not in session:
+        session['current_page_maxcount'] = 1
 
-    # FORM CTRL
+    submitted_title      = None
+    submitted_lang       = "native"
+    submitted_titlecount = 1
+    submitted_nav_ctrl   = 0 
+
+    # FORM
     form = SimpleForm()
     if request.method == "POST":
-        title_entered = form.title.data
-        lang_entered = form.lang.data
-        titlecount_entered = form.titleperpage.data
-
+        submitted_title      = form.title.data
+        submitted_lang       = form.lang.data
+        submitted_nav_ctrl   = int(form.page_nav.data) if form.page_nav.data else 0
+        submitted_titlecount = form.page_title_count.data
+    
     # API CALL
-    print("log: [", title_entered, "]")
-    data = api_get_data(title_entered, titlecount_entered); 
-    data_entered = [title_entered, lang_entered, titlecount_entered]
-    return render_template("index.html", media=data["data"]["Page"]["media"], form=form, data_entered=data_entered)
+    current_page_number   = session["current_page_number"]  
+    current_page_maxcount = session["current_page_maxcount"] 
+    api_data        = api_get_data(submitted_title, submitted_titlecount, current_page_number); 
+    form_data       = [submitted_title, submitted_lang, submitted_titlecount]
+    media_data      = api_data["data"]["Page"]["media"]
+    pagination_data = api_data["data"]["Page"]["pageInfo"]
+    current_page_maxcount  = pagination_data["total"] if pagination_data else 1
+    current_page_number    = clamp(current_page_number + submitted_nav_ctrl, 1, current_page_maxcount)
+    session["current_page_number"]   = current_page_number
+    session["current_page_maxcount"] = current_page_maxcount
+    # RENDER
+    return render_template("index.html", media_data=media_data, pagination_data=pagination_data, submitted_form_data=form_data, form=form)
 
 @app.route('/about')
 def about():
